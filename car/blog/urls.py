5b597be10c90193b361d59a3dbd72cfd2765346f
Mahdi <135426196+Mahdi-500@users.noreply.github.com>
2024-07-30 15:10:11 +0330

    added main page url

--- car/blog/urls.py
+++ car/blog/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 app_name = "blog"
 urlpatterns = [
-    path('', views.signupView, name="signup"),
-    path('\login', views.loginview, name='login'),
-    path('\<str:username>\createPost', views.AddPostview, name='add post'),
-    path('\<str:username>\profile', views.PostListview, name='profile'),
-    path('\<str:username>\post\<int:id>', views.PostDtailview, name='post detail')
+    path('CarBlog', views.Mainview, name="main"),
+    path('CarBlog\SignUp', views.signupView, name="signup"),
+    path('CarBlog\login', views.loginview, name='login'),
+    path('CarBlog\<str:username>\createPost', views.AddPostview, name='add post'),
+    path('CarBlog\<str:username>\profile', views.PostListview, name='profile'),
+    path('CarBlog\<str:username>\post\<int:id>', views.PostDtailview, name='post detail')
 ]
