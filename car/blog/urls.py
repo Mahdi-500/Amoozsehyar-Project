161576a688a63b5d1a2e838c3a21169113301c41
Mahdi <135426196+Mahdi-500@users.noreply.github.com>
2024-08-03 20:01:27 +0330

    added comment form url and also changed the main and signup urls

--- car/blog/urls.py
+++ car/blog/urls.py
@@ -3,11 +3,12 @@ from . import views
 
 app_name = "blog"
 urlpatterns = [
-    path('CarBlog', views.Mainview, name="main"),
-    path('CarBlog\SignUp', views.signupView, name="signup"),
+    path('CarBlog\<str:username>\global', views.Mainview, name="main"),
+    path('CarBlog', views.signupView, name="signup"),
     path('CarBlog\login', views.loginview, name='login'),
     path('CarBlog\<str:username>\createPost', views.AddPostview, name='add post'),
     path('CarBlog\<str:username>\profile', views.PostListview, name='profile'),
     path('CarBlog\<str:username>\post\<int:id>', views.PostDtailview, name='post detail'),
     path("CarBlog\Ticket", views.Ticketview, name='ticket'),
+    path('CarBlog\<str:username>\global\post\<int:id>\comment', views.Commentview, name="comment"),
 ]
