d62bb25b2c84e7a001bdc4e1627e3b5300b2ea60
Mahdi <135426196+Mahdi-500@users.noreply.github.com>
2024-08-05 19:28:09 +0330

    added reading time to post admin class

--- car/blog/admin.py
+++ car/blog/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+
+# ? admin actions
+@admin.action(description="mark all selected items as Accepted")
+def make_accept(modeladmin, request, queryset):
+    queryset.update(status="AC")
+
+# ? admin classes
 @admin.register(SignUp)
 class SignUpAdmin(admin.ModelAdmin):
     list_display = ['name', 'username', 'gender', 'joined']
@@ -9,10 +16,11 @@ class SignUpAdmin(admin.ModelAdmin):
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'author', 'status', 'created']
-    list_editable = ['status']
+    list_display = ['title', 'author', 'status', 'created', 'reading_time']
+    list_editable = ['status', "reading_time"]
     list_filter = ['status', 'created', 'author']
     raw_id_fields = ['author']
+    actions = [make_accept]
 
 @admin.register(Ticket)
 class TicketAdmin(admin.ModelAdmin):
@@ -24,3 +32,4 @@ class CommentAdmin(admin.ModelAdmin):
     list_display = ['author', 'created', 'status']
     list_editable = ['status']
     list_filter = ['created', 'status']
+    actions = ["make_accept"]
