6ab4a0d1a64df27b1a7aa63f16abfd537d8b4c91
Mahdi <135426196+Mahdi-500@users.noreply.github.com>
2024-09-02 19:33:55 +0330

    Revert "added markdown and bad words filter"
    This reverts commit 8b51b423e18c1fcb9335b060eae40505ebe685df.

--- car/blog/templatetags/blog_tags.py
+++ car/blog/templatetags/blog_tags.py
@@ -1,26 +1,9 @@
 from django import template
-from django.db.models import Max, Count
-from django.utils.safestring import mark_safe
 from ..models import *
-from markdown import markdown
+from django.db.models import Max
 
 register = template.Library()
 
 @register.simple_tag()
 def read_time():
     return Post.Publish.annotate(most_time=Max("reading_time")).order_by("-most_time")[:5]
-
-@register.simple_tag()
-def most_popular():
-    return Post.Publish.annotate(cc=Count("comment")).order_by("-cc")[:5]  # cc = comment_count
-
-@register.simple_tag()
-def comment_count(post):
-    return post.comment.count()
-
-banned_words = ['Fuck', 'bitch']
-@register.filter()
-def censore(text):
-    for bad in banned_words:
-        text = text.replace(bad, "***")
-    return mark_safe(markdown(text))
