a7a315e08f55c3680c93f1999b13348c11dade48
Mahdi <135426196+Mahdi-500@users.noreply.github.com>
2024-11-24 18:00:09 +0330

    added the edit post url patterns

--- car/blog/urls.py
+++ car/blog/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('CarBlog\<str:username>\global\post\<int:id>\comment', views.Commentview, name="comment"),
     path("CarBlog\search", views.SearchView, name="search"),
     path('CarBlog\delete_post\<int:id>', views.DeletePostView, name="delete post"),
+    path('CarBlog\edit_post\<int:id>', views.EditPostView, name='edit post')
 ]
